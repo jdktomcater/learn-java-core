461ce24c36bad4ede189339ea6a4b444aef580e1
jstimmy <jstimmy@itcom888.com>
2024-08-29 13:48:08 +0800

    测试用例补充

--- src/com/jdktomcat/pack/dsa/algorithm/book/offer/heap/p59/DataStreamKthLargestTest.java
+++ src/com/jdktomcat/pack/dsa/algorithm/book/offer/heap/p59/DataStreamKthLargestTest.java
@@ -9,5 +9,11 @@ public class DataStreamKthLargestTest {
     public void getKthLargest() {
         DataStreamKthLargest dataStreamKthLargest = new DataStreamKthLargest(5, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
         Assert.assertEquals(5, dataStreamKthLargest.getKthLargest());
+        dataStreamKthLargest.add(10);
+        Assert.assertEquals(6, dataStreamKthLargest.getKthLargest());
+        dataStreamKthLargest.add(11);
+        Assert.assertEquals(7, dataStreamKthLargest.getKthLargest());
+        dataStreamKthLargest.add(12);
+        Assert.assertEquals(8, dataStreamKthLargest.getKthLargest());
     }
 }
