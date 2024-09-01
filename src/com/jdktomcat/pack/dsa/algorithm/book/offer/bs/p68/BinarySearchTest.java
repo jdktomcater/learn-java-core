3891e8d3f44af41532ef3d61ccd2c1237bbf1b93
jstimmy <jstimmy@itcom888.com>
2024-09-01 12:23:55 +0800

    测试用例补充

--- src/com/jdktomcat/pack/dsa/algorithm/book/offer/bs/p68/BinarySearchTest.java
+++ src/com/jdktomcat/pack/dsa/algorithm/book/offer/bs/p68/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package com.jdktomcat.pack.dsa.algorithm.book.offer.bs.p68;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,9 @@ public class BinarySearchTest {
 
     @Test
     public void search() {
+        int[] datas = {1,2,3,4,6,7,8,9};
+        int target = 0;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(-1, index);
     }
 }
