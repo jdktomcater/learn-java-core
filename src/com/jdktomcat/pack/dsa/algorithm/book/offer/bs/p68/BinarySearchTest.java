67b56598969b2990a55f1a2b51f6a411c5fb3310
jstimmy <jstimmy@itcom888.com>
2024-09-01 14:13:50 +0800

    测试用例补充

--- src/com/jdktomcat/pack/dsa/algorithm/book/offer/bs/p68/BinarySearchTest.java
+++ src/com/jdktomcat/pack/dsa/algorithm/book/offer/bs/p68/BinarySearchTest.java
@@ -3,14 +3,52 @@ package com.jdktomcat.pack.dsa.algorithm.book.offer.bs.p68;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class BinarySearchTest {
 
     @Test
-    public void search() {
-        int[] datas = {1,2,3,4,6,7,8,9};
+    public void searchNormal() {
+        int[] datas = {1, 2, 3, 4, 6, 7, 8, 9};
+        int target = 3;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(2, index);
+    }
+
+    @Test
+    public void searchEdgeOnlyOne() {
+        int[] datas = {1};
+        int target = 1;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(0, index);
+    }
+
+    @Test
+    public void searchEdgeOnlyOneNotExist() {
+        int[] datas = {1};
         int target = 0;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(-1, index);
+    }
+
+    @Test
+    public void searchEdgeEmptyNotExist() {
+        int[] datas = {};
+        int target = 0;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(-1, index);
+    }
+
+    @Test
+    public void searchEdgeNotExistBeforeHead() {
+        int[] datas = {1, 2, 3, 4, 6, 7, 8, 9};
+        int target = 0;
+        int index = BinarySearch.search(datas, target);
+        Assert.assertEquals(-1, index);
+    }
+
+    @Test
+    public void searchEdgeNotExistAfterTail() {
+        int[] datas = {1, 2, 3, 4, 6, 7, 8, 9};
+        int target = 10;
         int index = BinarySearch.search(datas, target);
         Assert.assertEquals(-1, index);
     }
