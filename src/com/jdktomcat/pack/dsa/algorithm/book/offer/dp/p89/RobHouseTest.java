b2901dc9a2058f3685a89b4d6e424b5a0beca807
jstimmy <jstimmy@itcom888.com>
2024-09-07 07:23:40 +0800

    测试用例补充

---
+++ src/com/jdktomcat/pack/dsa/algorithm/book/offer/dp/p89/RobHouseTest.java
@@ -0,0 +1,26 @@
+package com.jdktomcat.pack.dsa.algorithm.book.offer.dp.p89;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RobHouseTest {
+
+    @Test
+    public void rob() {
+        int[] numbers = new int[]{2, 3, 4, 5, 3};
+        System.out.println("L1最大抢劫财产数：" + RobHouse.rob(numbers));
+    }
+
+    @Test
+    public void robL2() {
+        int[] numbers = new int[]{2, 3, 4, 5, 3};
+        System.out.println("L2最大抢劫财产数：" + RobHouse.robL2(numbers));
+    }
+
+    @Test
+    public void robL3() {
+        int[] numbers = new int[]{2, 3, 4, 5, 3};
+        System.out.println("L3最大抢劫财产数：" + RobHouse.robL3(numbers));
+    }
+}
---
+++ src/com/jdktomcat/pack/dsa/algorithm/book/offer/dp/p90/CircleRobHouseTest.java
@@ -0,0 +1,14 @@
+package com.jdktomcat.pack.dsa.algorithm.book.offer.dp.p90;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircleRobHouseTest {
+
+    @Test
+    public void rob() {
+        int[] numbers = new int[]{2, 3, 4, 5, 3};
+        System.out.println("环形最大抢劫财产数：" + CircleRobHouse.rob(numbers));
+    }
+}
