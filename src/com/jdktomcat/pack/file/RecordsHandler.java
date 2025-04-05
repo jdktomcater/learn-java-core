30cfa6837379f1757f9356a254fab61c15d7f81a
timmy <yfzf_timmy@zfopsmail.com>
2025-04-05 14:15:25 +0800

    架构设计图

--- src/com/jdktomcat/pack/file/RecordsHandler.java
+++ src/com/jdktomcat/pack/file/RecordsHandler.java
@@ -2,6 +2,7 @@ package com.jdktomcat.pack.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,14 +36,16 @@ public class RecordsHandler {
      *
      * @param records 数据
      */
-    private static void checkData(List<Record> records) {
+    private static void checkData(List<Record> records) throws FileNotFoundException {
         BigDecimal pre = records.get(0).getPre();
+        PrintWriter printWriter = new PrintWriter("out.txt");
         for (int i = 1; i < records.size(); i++) {
-            if(pre.compareTo(records.get(i).getPost()) != 0) {
-                System.out.println("行："+i+" 数据异常："+records.get(i).toString());
+            if (pre.compareTo(records.get(i).getPost()) != 0) {
+                printWriter.println("行：" + i + " 数据异常：" + records.get(i).toString());
             }
             pre = records.get(i).getPre();
         }
+        printWriter.flush();
     }
 
     /**
