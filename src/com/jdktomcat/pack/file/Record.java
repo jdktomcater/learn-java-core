ce45d2f85356a7b103809bfa3b244562ee4b05a3
timmy <yfzf_timmy@zfopsmail.com>
2025-04-07 14:38:09 +0800

    架构设计图

--- src/com/jdktomcat/pack/file/Record.java
+++ src/com/jdktomcat/pack/file/Record.java
@@ -1,35 +1,126 @@
 package com.jdktomcat.pack.file;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 记录
  */
 public class Record {
 
-    private final BigDecimal pre;
+    Long id;
 
-    private final BigDecimal post;
+    String orderId;
+
+    String merchantOrderId;
+
+    Integer merchantId;
+
+    Integer modificationType;
+
+    BigDecimal preBalance;
+
+    BigDecimal postBalance;
+
+    BigDecimal preAvailableBalance;
+
+    BigDecimal postAvailableBalance;
+
+    BigDecimal preFrozenBalance;
+
+    BigDecimal postFrozenBalance;
+
+    String remark;
+
+    String createAt;
+
+    String updateAt;
 
 
-    @Override
-    public String toString() {
-        return "Record{" +
-                "pre=" + pre +
-                ", post=" + post +
-                '}';
+    public Record(Long id,
+                  String orderId,
+                  String merchantOrderId,
+                  Integer merchantId,
+                  Integer modificationType,
+                  BigDecimal preBalance,
+                  BigDecimal postBalance,
+                  BigDecimal preAvailableBalance,
+                  BigDecimal postAvailableBalance,
+                  BigDecimal preFrozenBalance,
+                  BigDecimal postFrozenBalance,
+                  String remark,
+                  String createAt,
+                  String updateAt)
+    {
+        this.id = id;
+        this.orderId = orderId;
+        this.merchantOrderId = merchantOrderId;
+        this.merchantId = merchantId;
+        this.modificationType = modificationType;
+        this.preBalance = preBalance;
+        this.postBalance = postBalance;
+        this.preAvailableBalance = preAvailableBalance;
+        this.postAvailableBalance = postAvailableBalance;
+        this.preFrozenBalance = preFrozenBalance;
+        this.postFrozenBalance = postFrozenBalance;
+        this.remark = remark;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
     }
 
-    public BigDecimal getPre() {
-        return pre;
+    public Long getId() {
+        return id;
     }
 
-    public BigDecimal getPost() {
-        return post;
+    public String getOrderId() {
+        return orderId;
     }
 
-    public Record(BigDecimal pre, BigDecimal post) {
-        this.pre = pre;
-        this.post = post;
+    public String getMerchantOrderId() {
+        return merchantOrderId;
+    }
+
+    public Integer getMerchantId() {
+        return merchantId;
+    }
+
+    public Integer getModificationType() {
+        return modificationType;
+    }
+
+    public BigDecimal getPreBalance() {
+        return preBalance;
+    }
+
+    public BigDecimal getPostBalance() {
+        return postBalance;
+    }
+
+    public BigDecimal getPreAvailableBalance() {
+        return preAvailableBalance;
+    }
+
+    public BigDecimal getPostAvailableBalance() {
+        return postAvailableBalance;
+    }
+
+    public BigDecimal getPreFrozenBalance() {
+        return preFrozenBalance;
+    }
+
+    public BigDecimal getPostFrozenBalance() {
+        return postFrozenBalance;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public String getUpdateAt() {
+        return updateAt;
     }
 }
--- src/com/jdktomcat/pack/file/RecordsHandler.java
+++ src/com/jdktomcat/pack/file/RecordsHandler.java
@@ -1,12 +1,10 @@
 package com.jdktomcat.pack.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 记录处理
@@ -20,41 +18,68 @@ public class RecordsHandler {
      * @param filePath 文件路径
      * @return 记录列表
      */
-    private static List<Record> readData(String filePath) throws FileNotFoundException {
+    private static List<Record> readData(String filePath) throws FileNotFoundException, ParseException {
         List<Record> records = new ArrayList<>(100000);
         Scanner scanner = new Scanner(new File(filePath));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         while (scanner.hasNext()) {
-            String line = scanner.nextLine();
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
             String[] datas = line.split("\\s+");
-            records.add(new Record(new BigDecimal(datas[0].trim()), new BigDecimal(datas[1].trim())));
+            Long id = Long.parseLong(datas[0]);
+            String orderId = datas[1];
+            String merchantOrderId = datas[2];
+            Integer merchantId = Integer.parseInt(datas[3]);
+            Integer modificationType = Integer.parseInt(datas[4]);
+            BigDecimal preBalance = new BigDecimal(datas[5]);
+            BigDecimal postBalance = new BigDecimal(datas[6]);
+            BigDecimal preAvailableBalance = new BigDecimal(datas[7]);
+            BigDecimal postAvailableBalance = new BigDecimal(datas[8]);
+            BigDecimal preFrozenBalance = new BigDecimal(datas[9]);
+            BigDecimal postFrozenBalance = new BigDecimal(datas[10]);
+            String remark = datas[11];
+            String createAt = datas[12];
+            String updateAt = datas[13];
+            records.add(new Record(id, orderId, merchantOrderId, merchantId, modificationType, preBalance, postBalance, preAvailableBalance, postAvailableBalance, preFrozenBalance, postFrozenBalance, remark, createAt, updateAt));
         }
         return records;
     }
 
-    /**
-     * 检查数据
-     *
-     * @param records 数据
-     */
-    private static void checkData(List<Record> records) throws FileNotFoundException {
-        BigDecimal pre = records.get(0).getPre();
-        PrintWriter printWriter = new PrintWriter("out.txt");
-        for (int i = 1; i < records.size(); i++) {
-            if (pre.compareTo(records.get(i).getPost()) != 0) {
-                printWriter.println("行：" + i + " 数据异常：" + records.get(i).toString());
+    public static Set<String> checkOrder(List<Record> records) {
+        Set<String> orderIds = new HashSet<>();
+        for (int i = records.size() - 1; i >= 0; i--) {
+            Record record = records.get(i);
+            // 下单
+            if (record.getModificationType() == 10) {
+                orderIds.add(record.getOrderId());
             }
-            pre = records.get(i).getPre();
+            // 结算
+            if (record.getModificationType() == 11 || record.getModificationType() == 12) {
+                orderIds.remove(record.getOrderId());
+            }
         }
-        printWriter.flush();
+        return orderIds;
     }
+
+    public static void writeResult(Set<String> orderIds) throws IOException {
+        PrintWriter printWriter = new PrintWriter(new FileWriter("order_id_except.txt"));
+        orderIds.forEach(printWriter::println);
+        printWriter.flush();
+        printWriter.close();
+    }
+
 
     /**
      * 主函数入口
      *
      * @param args 参数
      */
-    public static void main(String[] args) throws FileNotFoundException {
-        String filePath = "C:\\timmy\\workspace\\learn-java-core\\txt\\data.txt";
-        RecordsHandler.checkData(readData(filePath));
+    public static void main(String[] args) throws IOException, ParseException {
+        String filePath = "C:\\timmy\\workspace\\learn-java-core\\txt\\records.txt";
+        List<Record> records = readData(filePath);
+        Set<String> orderIds = checkOrder(records);
+        writeResult(orderIds);
     }
 }
