3c40ae1e3a1e399dd190cee88316e536faa93efd
yuhata <jdktomcater@gmail.com>
2025-07-22 21:33:16 +0800

    架构图优化

---
+++ out/production/learn-java-core/com/jdktomcat/pack/dsa/algorithm/company/mihoyo/mid/SecondHighestSalary.sql
@@ -0,0 +1,11 @@
+-- 第二高的薪水
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary;
+
+
+
